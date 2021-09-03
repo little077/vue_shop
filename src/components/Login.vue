11bca8550b2ace9ef9e9fed875cf57f5a84024f3
little077 <1837687575@qq.com>
2021-09-03 10:42:26 +0800

    第二次

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
    <div class="login_container"> 
-       登录
+       <div class="login_box">
+         <div class="acatar_box">
+           <img src="~assets/logo.png">
+         </div>
+       </div>
    </div>
 </template>
 
@@ -13,5 +17,34 @@ export default {
 <style lang="less" scoped>
   .login_container{
       background: #2b4b6b;
+      height: 100%;
+  }
+  .login_box{
+    width:450px;
+    height: 300px;
+    background: #fff;
+    border-radius: 3px;
+    position: relative;
+    left: 50%;
+    top:50%;
+    transform: translate(-50%,-50%);
+      .acatar_box{
+        width: 130px;
+        height: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        position: relative;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        background: #fff;
+          img{
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background: #eee;
+          }
+      }
   }
 </style>
