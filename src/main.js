88e23b72177d0cb71c04285d7c9762378eb7b191
little077 <1837687575@qq.com>
2021-09-04 08:55:55 +0800

    修复

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+import './plugins/element'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 //导入基础样式
-import  './assets/css/global.css'
+import  'assets/css/global.css'
 import 'assets/fonts/iconfont.css'
-import { Message } from 'element-ui'
+import { Form, Message } from 'element-ui'
 /**网络模块 */
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/' 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
 import { Form, FormItem } from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css';
 import Input from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+
+
